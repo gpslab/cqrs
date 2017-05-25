99d109d5901eeef6008a690b5caee0f5bab1780e
Peter Gribanov <wcode404@gmail.com>
2017-05-25 11:47:12 +0300

    add php cs fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
 $file = __DIR__.'/../vendor/autoload.php';
 
 if (!file_exists($file)) {
