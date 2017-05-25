fef2cac35a1a6b4acd0966dcbf224aec25f8a990
Peter Gribanov <wcode404@gmail.com>
2017-05-25 16:07:33 +0300

    test DirectBindingQueryHandlerLocator

---
+++ tests/Query/Handler/Locator/DirectBindingQueryHandlerLocatorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Tests\Query\Handler\Locator;
+
+use GpsLab\Component\Query\Handler\Locator\DirectBindingQueryHandlerLocator;
+use GpsLab\Component\Query\Handler\QueryHandler;
+use GpsLab\Component\Query\Query;
+
+class DirectBindingQueryHandlerLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|Query
+     */
+    private $query;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|QueryHandler
+     */
+    private $handler;
+
+    /**
+     * @var DirectBindingQueryHandlerLocator
+     */
+    private $locator;
+
+    protected function setUp()
+    {
+        $this->query = $this->getMock(Query::class);
+        $this->handler = $this->getMock(QueryHandler::class);
+
+        $this->locator = new DirectBindingQueryHandlerLocator();
+    }
+
+    public function testGetQueryHandler()
+    {
+        $this->locator->registerHandler(get_class($this->query), $this->handler);
+
+        $handler = $this->locator->getQueryHandler($this->query);
+        $this->assertEquals($this->handler, $handler);
+    }
+
+    public function testNoQueryHandler()
+    {
+        $this->locator->registerHandler('foo', $this->handler);
+
+        $handler = $this->locator->getQueryHandler($this->query);
+        $this->assertNull($handler);
+    }
+}
