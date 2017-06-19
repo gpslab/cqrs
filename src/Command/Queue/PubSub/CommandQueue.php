5798c182848e29d3e49d8eb93ef881b97712b887
Peter Gribanov <wcode404@gmail.com>
2017-06-19 17:33:51 +0300

    create PubSub command queue interface

---
+++ src/Command/Queue/PubSub/CommandQueue.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Command\Queue\PubSub;
+
+use GpsLab\Component\Command\Command;
+
+/**
+ * Publish and Subscribe command queue
+ */
+interface CommandQueue
+{
+    /**
+     * Publish command to queue.
+     *
+     * @param Command $command
+     *
+     * @return bool
+     */
+    public function publish(Command $command);
+
+    /**
+     * Subscribe on command queue.
+     *
+     * @param callable $callback
+     */
+    public function subscribe(callable $callback);
+}
